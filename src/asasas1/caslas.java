2057eceedfe4040991dc3a333e8fd581bcfe6b3f
yhcho <whdudgh0214@gmail.com>
2021-05-19 12:29:22 +0900

    add "km", "ton", "분", "kbyte" in calc

--- src/asasas1/caslas.java
+++ src/asasas1/caslas.java
@@ -201,6 +201,11 @@ class calc implements ActionListener {
         JTextField textFieldNorth2;
         JButton button2;
         
+        private String first="";  		// 숫자 누적
+    	private String result=""; 		// 결과값  "=" 출력
+    	ArrayList<Integer> ee=new ArrayList<Integer>();  		// 값들 누적으로 받기.
+    	ArrayList<String> store=new ArrayList<String>();  		// 연산자 누적으로 저장.
+        
         panelf=new JPanel();
 		panel2=new JPanel();
 		panelNorth2=new JPanel();
@@ -224,7 +229,81 @@ class calc implements ActionListener {
 			button2=new JButton(strGrid2[i]);  				// 버튼 생성 
 			button2.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
+					String str=e.getActionCommand();  		// 문자열로 이벤트불러옴
+					String read;				// 텍스트필드에 있는 텍스트 읽기용도
 					
+					try{	
+						//연산기호가 아닌 숫자이면 true 이다.
+						if(str!="km" && str!= "ton"&& str!= "분"&& str!= "kbyte"&& str!= "C"&& str!= "="){	
+							textField2.setText(first);  		// 0을지우고 숫자누적하기위해 필요.
+							textField2.getText();  			// 누적 출력
+							textFieldNorth2.setText("");		// 연산자 텍스트 초기화
+							textFieldNorth2.getText(); 		// 출력
+							
+							read=textField2.getText();  		// 텍스트에 있는값 불러옴.
+							first=read+str;
+							textField2.setText(first);		// 누적 저장
+							textField2.getText(); 			// 누적 출력
+						}// if
+						
+						
+						
+						//연산기호가 들어왔을때 true.
+						if(str=="km" || str== "ton"|| str== "분"|| str== "kbyte"|| str== "="){
+							textFieldNorth2.setText(str); 				 // 연산자 기호 저장
+							textFieldNorth2.getText();  					// 연산자 기호 출력
+							
+							ee.add(Integer.parseInt(first));  // 연산하려는 값 하나씩 ArrayList에 저장
+							first="";						// 다시 누적하도록 초기화
+							
+							store.add(str); 			   // 연산자 ArrayList에 저장		
+						}// if
+						
+						//연산기호 "=" 결과값 눌렀을때.
+						if(str=="="){		
+							int sum=0;
+							sum=ee.get(0); 		// 최초 시작값을 받고 시작 , 1(+2)(+3)(+4)=result 초기값만 알면 3번만 "()" 연산해주면 된다. 
+							
+							for(int a=0,h=1; a<ee.size(); a++,h++){   // ArrayList 크기만큼 반복
+								int ve=store.size(); 				// 연산자 누적 횟수 저장
+								if(ve>0){							// 연산자 누적 모두 사용할때까지 반복.
+									ve--;			
+									//if문 또는 switch문으로 가능.
+									if(store.get(a)=="+"){			// 연산자 + 일때
+										sum=sum+ee.get(1);  			// (+2) 두번째 숫자와 연산
+									}else if(store.get(a)=="-"){	// 아래부터 반복..	
+										sum=sum-ee.get(1);	
+									}else if(store.get(a)=="X"){
+										sum=sum*ee.get(1);
+									}else if(store.get(a)=="/"){
+										try{				
+											sum=sum/ee.get(1);
+										}catch(Exception exc){		// 나눗셈은 0으로 나눌때 예외발생한다.
+											sum=0;  //에러발생시 0으로 출력.
+										}
+									}
+								}// if
+							}// for
+							result=sum+"";						//총 결과값
+							textField2.setText(result);  		// 결과값 저장
+							textField2.getText(); 				// 결과출력	
+						}// if
+						
+						//모든 값 초기화 시키고 다시 연산.
+						if(str=="C"){
+							first="";
+							textField2.setText("0"); 			 // 텍스트창 내용 지우고 0출력
+							textField2.getText();  			
+							textFieldNorth2.setText(""); 	// 연산자 텍스트창 초기화
+							textFieldNorth2.getText(); 
+							ee.clear();  					// 모든 요소들 제거
+							store.clear();  				// 모든 요소들 제거
+						}
+						
+					}catch(Exception ex){
+						textField2.setText("Error 다시입력 C클릭.");  // 텍스트창 내용 전부 지우기
+						textField2.getText(); 
+					}
 				}				//actionPerformed()
 			});	 			// 각 버튼마다 리스너 붙이기
 			button2.setBackground(new Color( 241,244,249));    // 버튼 집어넣기
